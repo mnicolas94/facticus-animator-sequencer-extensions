3f5497e42445eea0b59f3c00fdccef8dd3ea688e
mnicolas94 <miguelcok27@gmail.com>
2023-12-11 10:33:31 -0500

    feat: implement Tween.AsyncWaitForCompletion(CancellationToke) extension method

---
+++ Runtime/Extensions/TweenExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DG.Tweening;
+
+namespace AnimatorSequencerExtensions.Extensions
+{
+    public static class TweenExtensions
+    {
+        public static async Task AsyncWaitForCompletion(this Tween t, CancellationToken ct)
+        {
+            if (!t.active)
+            {
+                return;
+            }
+
+            while (t.active && !t.IsComplete() && !ct.IsCancellationRequested)
+            {
+                await Task.Yield();
+            }
+
+            if (ct.IsCancellationRequested)
+            {
+                t.Complete(true);
+            }
+        }
+    }
+}
