a527fe5359e3ecc5c1c330848055f3820f34c53e
mnicolas94 <miguelcok27@gmail.com>
2024-06-22 20:23:51 -0400

    feat: add AnimationSequencerController.PlayAndWaitAsync extension method

---
+++ Runtime/Extensions/AnimationSequencerControllerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using BrunoMikoski.AnimationSequencer;
+
+namespace AnimatorSequencerExtensions.Extensions
+{
+    public static class AnimationSequencerControllerExtensions
+    {
+        public static async Task PlayAndWaitAsync(this AnimationSequencerController animation, CancellationToken ct)
+        {
+            animation.Play();
+            await animation.PlayingSequence.AsyncWaitForCompletion(ct);
+        }
+    }
+}
